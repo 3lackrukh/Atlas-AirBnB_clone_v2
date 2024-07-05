2deb0904d326e79468389097b6b03e14ce919bdd
3lackrukh <nathan.rhys@atlasschool.com>
2024-07-05 17:36:25 +0000

    removed tags

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 """ App routing and method definition"""
 @app.route('/', strict_slashes=False)
 def hello_hbnb ():
-    return '<p> Hello, HBNB! </p>'
+    return 'Hello, HBNB!'
 
 if __name__ == '__main__':
     app.run (host='0.0.0.0', port=5000)
