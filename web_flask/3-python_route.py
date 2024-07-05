37adc7eb254d9005f27b3a003e9929159d2237ba
3lackrukh <nathan.rhys@atlasschool.com>
2024-07-05 18:04:02 +0000

    two routes?

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -23,7 +23,7 @@ def c_text(text):
     return 'C {}'.format(text.replace('_', ' '))
 # replace underscore with space
 
-
+@app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text='is cool'):
     return 'Python {}'.format(text.replace('_', ' '))
